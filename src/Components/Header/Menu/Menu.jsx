635f3e7a1a44e0e9dc9e4b76468b8282748b464f
dnlMatos <georginedanilo@gmail.com>
2023-09-13 12:00:41 -0300

    evolucao2

--- src/Components/Header/Menu/Menu.jsx
+++ src/Components/Header/Menu/Menu.jsx
@@ -7,60 +7,51 @@ export default function Menu() {
   const [installment, setInstallment] = useState(5);
   const [payment, setPayment] = useState(200);
   const [month, setMonth] = useState();
-  
+  const [months, setMonths] = useState([]);
+  const allMonths = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
+
   const date = new Date();
   const monthString = (date.getMonth() + 1).toString();
   const dayString = date.getDate().toString();
   const yearString = date.getFullYear().toString();
-  
+
   const newFormatDay = parseInt(dayString);
   const newFormatMonth = parseInt(monthString);
   const newFormatYear = parseInt(yearString);
-  
-  useEffect(()=>{
-    checkMonth(newFormatMonth)
-  },[])
 
-  const checkMonth = (month) => {
-    switch (month) {
-      case 1:
-        setMonth("Janeiro");
-        break;
-      case 2:
-        setMonth("Fevereiro");
-        break;
-      case 3:
-        setMonth("Março");
-        break;
-      case 4:
-        setMonth("Abril");
-        break;
-      case 5:
-        setMonth("Maio");
-        break;
-      case 6:
-        setMonth("Junho");
-        break;
-      case 7:
-        setMonth("Julho");
-        break;
-      case 8:
-        setMonth("Agosto");
-        break;
-      case 9:
-        setMonth("Setembro");
-        break;
-      case 10:
-        setMonth("Outubro");
-        break;
-      case 11:
-        setMonth("Novembro");
-        break;
-      case 12:
-        setMonth("Dezembro");
-        break;
-      default:
+  useEffect(() => {
+    checkingPresentMonth()
+    chechMonth();
+  }, []);
+
+  const checkingPresentMonth = () => {
+    {
+      newFormatDay <= 5
+        ? setMonth(newFormatMonth)
+        : setMonth(newFormatMonth + 1);
     }
+  };
+
+  console.log(month);
+
+  const chechMonth = () => {
+    const listMonths = allMonths.filter((month) => {
+     return month > newFormatMonth
+    });
+    console.log(listMonths);
   };
 
   return (
@@ -72,7 +63,7 @@ export default function Menu() {
         {newFormatDay <= 5 ? (
           <option value={month}>{month}</option>
         ) : (
-          <option value={newFormatMonth + 1}>{newFormatMonth + 1}</option>
+          <option value={newFormatMonth + 1}>{month}</option>
         )}
       </select>
     </span>
